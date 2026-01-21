bf216e1a6ab0ac6858381f61c3b2a9887cfb1247
wiktoriasikerko05 <wiktoriasikerko@outlook.com>
2026-01-21 12:10:41 +0100

    ShopContext.jsx

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -6,10 +6,10 @@ export const ShopProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [favorites, setFavorites] = useState([]);
 
-  // 1. Dodawanie do koszyka (z uwzględnieniem rozmiaru)
+  // 1. Dodawanie do koszyka
   const addToCart = (product, size = 'M') => {
     setCart((prevCart) => {
-      // Sprawdzamy, czy ten produkt W TYM ROZMIARZE już jest
+    
       const existingItem = prevCart.find((item) => item.id === product.id && item.selectedSize === size);
       
       if (existingItem) {
@@ -19,17 +19,17 @@ export const ShopProvider = ({ children }) => {
             : item
         );
       }
-      // Jeśli nie ma, dodajemy jako nowy
+    
       return [...prevCart, { ...product, quantity: 1, selectedSize: size }];
     });
   };
 
-  // 2. Usuwanie z koszyka (konkretny rozmiar)
+  // 2. Usuwanie z koszyka
   const removeFromCart = (id, size) => {
     setCart((prevCart) => prevCart.filter((item) => !(item.id === id && item.selectedSize === size)));
   };
 
-  // 3. Ulubione (Serduszko)
+  // 3. Ulubione
   const toggleFavorite = (product) => {
     setFavorites((prev) => {
       const isFavorite = prev.find(item => item.id === product.id);
@@ -40,20 +40,20 @@ export const ShopProvider = ({ children }) => {
     });
   };
 
-  // 4. Konwerter Walut (BEZ GROSZY - Pełne Złotówki)
+  // 4. Konwerter Walut
   const formatPrice = (priceInUSD) => {
-    // Math.round zaokrągla do najbliższej liczby całkowitej
+  
     const priceInPLN = Math.round(priceInUSD * 4.1); 
     
     return new Intl.NumberFormat('pl-PL', { 
       style: 'currency', 
       currency: 'PLN',
-      minimumFractionDigits: 0, // Zero miejsc po przecinku
+      minimumFractionDigits: 0, 
       maximumFractionDigits: 0 
     }).format(priceInPLN);
   };
 
-  // Liczenie sumy i ilości
+
   const cartTotal = cart.reduce((total, item) => total + item.price * item.quantity, 0);
   const cartCount = cart.reduce((count, item) => count + item.quantity, 0);
 
@@ -66,5 +66,6 @@ export const ShopProvider = ({ children }) => {
     </ShopContext.Provider>
   );
 };
+
 
 export const useShop = () => useContext(ShopContext);
