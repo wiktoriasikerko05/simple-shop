f3053b12835e35ac481180e9d6e143678c021b5e
wiktoriasikerko05 <wiktoriasikerko@outlook.com>
2026-01-21 12:18:04 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' /* <--- TO JEST KLUCZOWE */
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ShopProvider } from './context/ShopContext'
 
